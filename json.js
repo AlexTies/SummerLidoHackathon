6a02f3a670fb799e154bd4ae6cb082f860f2e4b0
AlexTies <alexties99@gmail.com>
2018-08-04 10:42:50 +0200

    Update 1

---
+++ json.js
@@ -0,0 +1,122 @@
+var ToExplore = [
+  {
+  "id":"ABC",
+  "type":"explore",
+  "name":"Südtiroler Archäologiemuseum",
+  "weil":"BECAUSE YOU VISITED MUSEION BOZEN",
+  "bildurl":"arch.jpg",
+  "themen":[
+    "kultur", "archäologie", "museum", "highlight"
+  ],
+  "location":[
+    "bozen"
+  ],
+  "Preis":"1.50€ - 9€",
+  "Beschreibung_kurz":"Das Südtiroler Archäologiemuseum ist ein archäologisches Museum in Bozen. Das Museum ist der Ausstellungsort des „Mannes vom Tisenjoch“, besser bekannt als „Ötzi“.",
+  "Beschreibung_lang":"Neben der Gletschermumie „Ötzi“ nebst dessen Beifunden präsentiert das Museum bedeutende Funde aus dem Südtiroler Raum. Die ältesten Exponate stammen aus der Altsteinzeit, die jüngsten aus der Karolingerzeit. Modelle, Rekonstruktionen, Raumbilder, Videos und interaktive Multimediastationen geben einen Einblick in die frühe Vergangenheit des südlichen Alpenraumes. Die Ausstellungsräume sind diachronisch angeordnet. So befindet sich Ötzi und seine Zeit im ersten Stock, während sich die Fundstücke aus der römischen Antike und der Völkerwanderungszeit im dritten Stock befinden."
+
+},{
+  "id":"DFG",
+  "type":"explore",
+  "name":"Botanische Gärten Trauttmannsdorff",
+  "bildurl":"traut.jpg",
+  "weil":"",
+  "themen":[
+    "natur", "highlight"
+  ],
+  "location":[
+    "meraner land"
+  ],
+  "Preis":"3.5€ - 13€",
+  "Beschreibung_kurz":"Wunderschöne Gartenlandschaften mit verschiedensten Pflanzen aus aller Welt.",
+  "Beschreibung_lang":"Die Gärten von Schloss Trauttmansdorff in Meran erstrecken sich auf einer Fläche von 12 Hektar in Form eines natürlichen Amphitheaters über einen Höhenunterschied von 100 Metern. Das eröffnet beeindruckende Perspektiven auf exotische Gartenlandschaften sowie atemberaubende Ausblicke auf die umliegende Bergwelt und die Kurstadt Meran."
+},
+{
+  "id":"DFC",
+  "type":"explore",
+  "name":"Hirzer Seilbahn",
+  "bildurl":"hirzer.jpg",
+  "weil":"",
+  "themen":[
+    "natur", "highlight, wandern"
+  ],
+  "location":[
+    "meraner land"
+  ],
+  "Preis":"9",
+  "Beschreibung_kurz":"Wunderschöne Wanderung mit Aussicht und Einkehrmöglichkeit",
+  "Beschreibung_lang":"Route: Sie fahren mit der Hirzer Seilbahn bis zur Bergstation Klammeben (1980m). Von dort steigen Sie über den Weg Nr. 6 ab zum Gasthof Grube (1808m). Der gut markierte Weg führt Sie weiter nach Oberkirn (1458m) und schließlich weiter bis zur Mittelstation in Prenn (1404m). Von dort aus fahren Sie mit der Seilbahn zurück ins Tal. Leichte Wanderung; Gehzeit: ca. 2 Stunden."
+},
+{
+  "id":"ABA",
+  "type":"explore",
+  "name":"Volkskundemuseum Dietenheim",
+  "weil":"BECAUSE YOU LOVE HISTORY AND CULTURE",
+  "bildurl":"diet.jpg",
+  "themen":[
+    "kultur", "archäologie", "museum", "highlight","history"
+  ],
+  "location":[
+    "dietenheim"
+  ],
+  "Preis":"1.50€ - 9€",
+  "Beschreibung_kurz":"Das Südtiroler Landesmuseum für Volkskunde veranschaulicht Geschichte und Kultur des bäuerlichen und handwerklichen Lebens.",
+  "Beschreibung_lang":"Das Volkskundemuseum in Dietenheim bei Bruneck liegt im Herzen des Pustertales. Es zeigt die Lebenswelt der ländlichen Bevölkerung von einst – vom Landadeligen über den selbstversorgenden Bauern bis zum Tagelöhner. Mittelpunkt ist der barocke Ansitz Mair am Hof mit den herrschaftlichen Räumen und den volkskundlichen Sammlungen. Das dazugehörige Wirtschaftsgebäude beherbergt eine Vielzahl von landwirtschaftlichen Geräten und Gefährten. Das Freigelände mit alten originalen Bauernhäusern, Handwerksstätten, Bauerngärten und Haustieren lädt zum Verweilen ein und erzählt vom Alltag in früheren Tagen."
+
+}
+];
+
+var saved = [
+  {
+  "id":"ghj",
+  "type":"saved",
+  "name":"Therme Meran",
+  "weil":"",
+  "bildurl":"therme.jpg",
+  "memoriesVA":["04.08.2018"],
+  "themen":[
+    "relaxation", "wellness", "swimming"
+  ],
+  "location":[
+    "meran"
+  ],
+  "Preis":"about 20€/Person",
+  "Beschreibung_kurz":"At Terme Merano/Therme Meran you can find peace and relaxation in the indoor and outdoor pools, saunas, massive park and do something good for your health."
+},
+  {
+  "id":"ghb",
+  "type":"saved",
+  "name":"acquarena",
+  "weil":"",
+  "bildurl":"acquarena.jpg",
+  "memoriesVA":["04.08.2018"],
+  "themen":[
+    "relaxation", "wellness", "swimming"
+  ],
+  "location":[
+    "brixen"
+  ],
+  "Preis":"about 9 - 22€/Person",
+  "Beschreibung_kurz":"Exzellentes Freibad mit großzügigem SPA."
+}
+];
+
+var memories = [
+  {
+  "id":"ghj",
+  "type":"memory",
+  "name":"Museion",
+  "weil":"",
+  "bildurl":"museion.jpg",
+  "gallerie":["museion1.jpg","museion2.jpg","museion3.jpg"],
+  "memoriesVA":["04.08.2018"],
+  "themen":[
+    "museum", "modern art"
+  ],
+  "location":[
+    "bozen"
+  ],
+  "Preis":"1.50€ - 9€",
+  "Beschreibung_kurz":"Museion Bozen/Bolzano exhibits, preserves and communicates works of modern and contemporary art using an innovative, interdisciplinary approach."
+}
+];
